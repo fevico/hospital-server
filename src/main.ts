cc7e68e3806ca3d968c23ea097367018cd23a425
fevico <ajayifavour81@gmail.com>
2026-01-16 13:23:05 +0100

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || '*',
+    origin: [
+    'https://hospital-client-opal.vercel.app',
+    'http://localhost:5173', // keep for local dev
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
